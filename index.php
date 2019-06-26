0d9626d79edc2d60d7a55db38959f9ad2e55e537
romasmi <romasmi@ro.ru>
2019-06-26 16:03:44 +0300

    Link to telegram api, add test functional

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
 
-$userName = isset($_GET['name']) ? ', ' . $_GET['name'] : '';
+require_once('vendor/autoload.php');
 
-echo "Hello world{$userName}";
+use Telegram\Bot\Api;
+
+$telegram = new Api('895714546:AAHdtxNN2dTVX03xP5F4hc7WJmfHXq6dzak');
+$result = $telegram -> getWebhookUpdates();
+
+$text = $result["message"]["text"];
+$chat_id = $result["message"]["chat"]["id"];
+$name = $result["message"]["from"]["username"];
+
+$keyboard = [["Answer me"]];
+
+if ($text)
+{
+    $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Hello, {$name}" ]);
+}
+else
+{
+    $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Send some message" ]);
+}
+
