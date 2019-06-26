5bf6bd75d8e60503b39f0ba555fb5d2745e43560
romasmi <romasmi@ro.ru>
2019-06-26 17:51:09 +0300

    Add /start functional

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once('vendor/autoload.php');
 use Telegram\Bot\Api;
 
 $telegram = new Api('895714546:AAHdtxNN2dTVX03xP5F4hc7WJmfHXq6dzak');
-$result = $telegram -> getWebhookUpdates();
+$result = $telegram->getWebhookUpdates();
 
 $text = $result["message"]["text"];
 $chat_id = $result["message"]["chat"]["id"];
@@ -13,12 +13,27 @@ $name = $result["message"]["from"]["username"];
 
 $keyboard = [["Answer me"]];
 
-if ($text)
-{
-    $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Hello, {$name}" ]);
-}
-else
-{
-    $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Send some message" ]);
+if ($text) {
+    switch ($text) {
+        case '/start':
+            {
+                $telegram->sendMessage(['chat_id' => $chat_id, 'text' => "Hello, {$name}"]);
+                $telegram->replyKeyboardMarkup
+                (
+                    [
+                        'keyboard' => [['Найти тур на конкретную дату'], ['Найти тур в интервал дат']],
+                        'resize_keyboard' => true,
+                        'one_time_keyboard' => false
+                    ]
+                );
+                break;
+            }
+        default:
+            $telegram->sendMessage(['chat_id' => $chat_id, 'text' => "Invalid command"]);
+    }
+
+
+} else {
+    $telegram->sendMessage(['chat_id' => $chat_id, 'text' => "Send some message"]);
 }
 
